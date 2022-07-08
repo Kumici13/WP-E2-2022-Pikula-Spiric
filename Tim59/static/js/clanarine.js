aaf98cb274db2dcd2d21450abab768ec40fa6873
spiricnra2052019 <spiricn00@gmail.com>
2022-07-08 02:27:33 +0200

    Clanarine

---
+++ Tim59/static/js/clanarine.js
@@ -0,0 +1,68 @@
+new Vue({
+    el: '#clanarine-app',
+    data:   
+	{
+		clanarine: [],
+		//changeActivityOfClanarine: [],
+        pretragaClanarine: '',
+      
+      
+    },
+    mounted()  
+	{
+        axios
+            .get('app/getclanarine')
+            .then(response => 
+			{
+                this.clanarine = response.data;
+            })
+            .catch(error => 
+			{
+                console.log(error);
+                alert(error.response.data.sadrzaj);
+            });
+    },
+    methods:    
+	{
+        ulogovanKorisnik: function()    
+		{
+            return window.localStorage.getItem('jwt') != null;
+        },
+        
+   /*    changeActivityOfClanarine: function(regKorisnik)
+       {
+		this.korisnickoIme =  regKorisnik 
+		
+		axios
+            .post('app/changeActivityOfClanarine' , this.korisnickoIme )
+            .then(response => 
+			{
+                this.changeActivityOfUser = response.data;
+            })
+            .catch(error => 
+			{
+                console.log(error);
+                
+            });
+	
+	   } */
+       	
+    },
+    computed:   
+	{
+        clanarineFilter: function() 
+		{
+            return this.clanarine.filter((objekat) => 
+			{	
+                return 	   ((objekat.datumPlacanja.toLowerCase().match(this.pretragaClanarine.toLowerCase()))
+						 )
+											
+				});
+        }
+    }
+	
+});
+
+
+
+
